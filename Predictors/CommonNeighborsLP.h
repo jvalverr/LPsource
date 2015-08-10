589229b16e73956874e3d3c72621b81c2c0634f9
Jorge Valverde-Rebaza <jorge.carlos14@gmail.com>
2015-08-10 19:30:11 -0300

    Create CommonNeighborsLP.h

---
+++ Predictors/CommonNeighborsLP.h
@@ -0,0 +1,34 @@
+/*
+------------------------------------------------
+Copyright (C) 2015 by Jorge C. Valverde-Rebaza
+Email: jvalverr@icmc.usp.br
+This file is part of LPsource.
+
+LPsource is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+LPsource is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should
+have received a copy of the GNU General Public License along with LPsource. If not, see <http://www.gnu.org/licenses/>.
+------------------------------------------------
+*/
+
+
+#ifndef COMMONNEIGHBORSLP_H_INCLUDED
+#define COMMONNEIGHBORSLP_H_INCLUDED
+
+
+
+#include "../Network.h"
+#include "LinkPredictor.h"
+
+class CommonNeighborsLP : public LinkPredictor {
+	private:
+	protected:
+	public:
+		CommonNeighborsLP( const Network& );
+		~CommonNeighborsLP();
+		double generateScore( index_v, index_v );
+};
+
+
+#endif // COMMONNEIGHBORSLP_H_INCLUDED
