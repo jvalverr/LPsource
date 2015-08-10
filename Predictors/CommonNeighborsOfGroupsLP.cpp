090dba3119cee4ad17c7617d1a1f61fb6d106874
Jorge Valverde-Rebaza <jorge.carlos14@gmail.com>
2015-08-10 20:56:53 -0300

    Update CommonNeighborsOfGroupsLP.cpp

--- Predictors/CommonNeighborsOfGroupsLP.cpp
+++ Predictors/CommonNeighborsOfGroupsLP.cpp
@@ -1,8 +1,23 @@
+/*
+------------------------------------------------
+Copyright (C) 2015 by Jorge C. Valverde-Rebaza
+Email: jvalverr@icmc.usp.br
+This file is part of LPsource.
+
+The code to calculate this link prediction measure is based on the paper [1].
+
+[1] Jorge Valverde-Rebaza and Alneu Lopes. "Link Prediction in Online Social Networks Using Group Information". The 14th
+    International Conference on Computational Science and Its Applications (ICCSA 2014). Guimarães, Portugal, July 2014.
+
+LPsource is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+LPsource is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should
+have received a copy of the GNU General Public License along with LPsource. If not, see <http://www.gnu.org/licenses/>.
+------------------------------------------------
+*/
 
 
-
-
-#include <math.h>
 #include "CommonNeighborsOfGroupsLP.h"
 
 CommonNeighborsOfGroupsLP::CommonNeighborsOfGroupsLP( const Network& network ): LinkPredictor(network) {
