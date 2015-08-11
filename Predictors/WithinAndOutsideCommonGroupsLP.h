e158b979d0f949359feba559de1242a0320c11c7
Jorge Valverde-Rebaza <jorge.carlos14@gmail.com>
2015-08-10 22:15:19 -0300

    Create WithinAndOutsideCommonGroupsLP.h

---
+++ Predictors/WithinAndOutsideCommonGroupsLP.h
@@ -0,0 +1,32 @@
+/*
+------------------------------------------------
+Copyright (C) 2015 by Jorge C. Valverde-Rebaza
+Email: jvalverr@icmc.usp.br
+This file is part of LPsource.
+
+LPsource is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+LPsource is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should
+have received a copy of the GNU General Public License along with LPsource. If not, see <http://www.gnu.org/licenses/>.
+------------------------------------------------
+*/
+
+
+#ifndef WITHINANDOUTSIDECOMMONGROUPSLP_H_INCLUDED
+#define WITHINANDOUTSIDECOMMONGROUPSLP_H_INCLUDED
+
+
+#include "../Network.h"
+#include "LinkPredictor.h"
+
+class WithinAndOutsideCommonGroupsLP : public LinkPredictor {
+	private:
+	protected:
+	public:
+		WithinAndOutsideCommonGroupsLP( const Network& );
+		~WithinAndOutsideCommonGroupsLP();
+		double generateScore( index_v, index_v );
+};
+
+#endif // WITHINANDOUTSIDECOMMONGROUPSLP_H_INCLUDED
